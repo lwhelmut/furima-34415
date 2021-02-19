6942556c114a765247ba595ee0d957b83d59e2aa
lwhelmut <wilkeleo.1021@gmail.com>
2021-02-19 11:42:21 +0900

    third commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :record
+  # has_one :record
   has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -14,14 +14,15 @@ class Item < ApplicationRecord
     validates :item_name
     validates :item_explanation
     validates :image
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_price_responsibility_id
+      validates :prefecture_id
+      validates :shipping_date_forecast_id
+    end
+    validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+    validates :item_price, numericality: { with: /\A[0-9]+\z/, message: 'には半角数値を使用してください' }
   end
-  with_options presence: true, numericality: { other_than: 1 } do
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_price_responsibility_id
-    validates :prefecture_id
-    validates :shipping_date_forecast_id
-  end
-  validates :item_price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
-  validates :item_price, numericality: { with: /\A[0-9]+\z/, message: 'には半角数値を使用してください' }
 end
