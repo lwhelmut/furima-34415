979917451f674a200aa23e6ad50811856e008cac
lwhelmut <wilkeleo.1021@gmail.com>
2021-02-24 14:12:31 +0900

    fixed4 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sir_name, :name, :sir_name_reading, :name_reading, :birth_date])
   end
 
-  #def basic_auth
-    #authenticate_or_request_with_http_basic do |username, password|
-      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    #end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
